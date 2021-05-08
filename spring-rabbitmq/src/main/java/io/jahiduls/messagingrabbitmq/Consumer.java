a7da3a160c16611d6320d48239dac67eacee220d
Jahid <jahid90@users.noreply.github.com>
2021-05-08 22:33:13 +0530

    [spring-rabbitmq] implement sample producer and consumer

---
+++ spring-rabbitmq/src/main/java/io/jahiduls/messagingrabbitmq/Consumer.java
@@ -0,0 +1,18 @@
+package io.jahiduls.messagingrabbitmq;
+
+import java.util.concurrent.CountDownLatch;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Consumer {
+    private CountDownLatch latch = new CountDownLatch(1);
+
+    public void receiveMessage(String message) {
+        System.out.println(" [x] Received <" + message + ">");
+        latch.countDown();
+    }
+
+    public CountDownLatch getLatch() {
+        return latch;
+    }
+}
---
+++ spring-rabbitmq/src/main/java/io/jahiduls/messagingrabbitmq/Producer.java
@@ -0,0 +1,26 @@
+package io.jahiduls.messagingrabbitmq;
+
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Producer implements CommandLineRunner {
+
+    private final RabbitTemplate rabbitTemplate;
+    private final Consumer consumer;
+
+    public Producer(Consumer consumer, RabbitTemplate rabbitTemplate) {
+        this.consumer = consumer;
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println(" [x] Sending message...");
+        rabbitTemplate.convertAndSend(MessagingRabbitmqApplication.topicExchangeName, "foo.bar.baz", "Hello from Spring RabbitMQ!");
+        consumer.getLatch().await(10000, TimeUnit.MILLISECONDS);
+    }
+}
